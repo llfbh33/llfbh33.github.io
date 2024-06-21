e21376e6bf53cac90681381f10cb4dda415e018b
Aubrie <aubriewoodbine@yahoo.com>
2024-06-21 10:19:42 -0500

    adding slideshow

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,19 +1,49 @@
-let img = document.getElementById('capstone');
-console.log(img)
 
-var slides=['image/capstone-edit-page.png','image/capstone-landing-page.png'];
+// For first project
+const img = document.getElementById('capstone-image');
 
-var Start=0;
+let slides=[
+    'https://profile-images-pencrafted-capstone.s3.us-west-2.amazonaws.com/capstone-edit-page.png',
+    'https://profile-images-pencrafted-capstone.s3.us-west-2.amazonaws.com/Screenshot+2024-06-20+190036.png',
+    'https://profile-images-pencrafted-capstone.s3.us-west-2.amazonaws.com/Screenshot+2024-06-20+185307.png',
+    'https://profile-images-pencrafted-capstone.s3.us-west-2.amazonaws.com/capstone-landing-page.png',
+
+];
+
+let start=0;
 
 function slider(){
-    if(Start<slides.length){
-        Start=Start+1;
+    img.style.opacity
+    if(start<slides.length){
+        start=start+1;
     }
     else{
-        Start=1;
+        start=1;
     }
     console.log(img);
-    img.innerHTML = "<img src="+slides[Start-1]+">";
-
+    img.src = slides[start-1]
 }
-setInterval(slider,2000);
+setInterval(slider,2500);
+
+// For second project
+const img2 = document.getElementById('group-image');
+
+let slides2=[
+
+
+];
+
+let start2=0;
+
+function slider2(){
+    img2.style.opacity
+    if(start2<slides2.length){
+        start2=start2+1;
+    }
+    else{
+        start2=1;
+    }
+    console.log(img2);
+    img2.src = slides2[start2-1]
+}
+setInterval(slider2,2500);
